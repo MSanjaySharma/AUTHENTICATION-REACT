90e8caaf485587181ed692bc924874b1cd3d95c4
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-08-03 11:26:14 +0530

    Update imports

--- config/routes.js
+++ config/routes.js
@@ -4,10 +4,6 @@ const router = express.Router();
 const { authenticateUser } = require("../app/middlewares/authentication");
 
 const usersController = require("../app/controllers/usersController");
-const customersController = require("../app/controllers/customersController");
-const departmentsController = require("../app/controllers/departmentsController");
-const employeesController = require("../app/controllers/employeesController");
-const ticketsController = require("../app/controllers/ticketsController");
 
 router.post("/users/registration", usersController.register);
 router.post("/users/login", usersController.login);
