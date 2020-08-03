7ad16527eef4fc7512ad8c6471631009ca59f916
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-08-03 11:25:43 +0530

    Updates on cust

--- config/routes.js
+++ config/routes.js
@@ -14,11 +14,4 @@ router.post("/users/login", usersController.login);
 router.get("/users/account", authenticateUser, usersController.account);
 router.delete("/users/logout", authenticateUser, usersController.logout);
 
-router.get("/customers", authenticateUser, customersController.list);
-router.get("/customers/:id", authenticateUser, customersController.show);
-router.post("/customers", authenticateUser, customersController.create);
-router.put("/customers/:id", authenticateUser, customersController.update);
-router.delete("/customers/:id", authenticateUser, customersController.delete);
-
-
 module.exports = router;
