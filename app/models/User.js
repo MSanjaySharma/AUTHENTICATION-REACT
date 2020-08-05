7afe3f2bce601d571b525c7f243587025aa634cf
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-08-05 12:11:05 +0530

    Update Routes

--- app/models/User.js
+++ app/models/User.js
@@ -22,6 +22,8 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+      minlength: 8,
+      maxlength: 64,
     },
   },
   { timestamps: true }
