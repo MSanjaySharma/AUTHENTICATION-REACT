ccddec59f58cb3c9fdee371f68e287da83c25fb5
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-08-05 12:06:36 +0530

    Update Routes

--- config/routes.js
+++ config/routes.js
@@ -7,7 +7,6 @@ const usersController = require("../app/controllers/usersController");
 
 router.post("/users/registration", usersController.register);
 router.post("/users/login", usersController.login);
-router.get("/users/account", authenticateUser, usersController.account);
 router.delete("/users/logout", authenticateUser, usersController.logout);
 
 module.exports = router;
