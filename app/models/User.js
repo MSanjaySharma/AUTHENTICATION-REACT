cb45c204ebb7863d97efe20260cd4f7dca2e49ce
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-08-05 12:09:53 +0530

    Update Models

---
+++ app/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+      maxlength: 32,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
